a691cd868c9d005374891fc777a6c92f8ff70266
real457 <myrealme19999@gmail.com>
2026-05-15 08:34:11 -0700

    Initial app upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+
+const CACHE_NAME = 'healthwave-v1';
+const assets = ['/'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
